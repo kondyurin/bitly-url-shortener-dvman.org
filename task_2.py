5cef599334618586d64661081bcafc4638091208
kondyurin <a.kondyurin@yandex.ru>
2019-04-22 08:34:59 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -4,10 +4,7 @@ from dotenv import load_dotenv
 
 
 def get_shorten_url(token, url):
-    """
-    Ф-ия принимает токен, url
-    Ф-ия возвращает битлинк
-    """
+    """Return bitlink from url"""
     payload = {
         'long_url': url
     }
@@ -16,19 +13,15 @@ def get_shorten_url(token, url):
     }
     url = 'https://api-ssl.bitly.com/v4/shorten'
     response = requests.post(url, headers=headers, json=payload)
-    if response.ok:
-        data = response.json()
-        shorten_url = data['link']
-    else:
-        shorten_url = "Incorrect url"
+    if not response.ok:
+        return
+    data = response.json()
+    shorten_url = data['link']
     return shorten_url
 
 
 def get_url_clicks(shorten_url):
-    """
-    Ф-ия принимает битлинк
-    Ф-ия возвращает суммарное кол-во кликов
-    """
+    """Return bitlink clicks count"""
     params = {
         'units': -1,
         'unit': 'day'
@@ -38,28 +31,21 @@ def get_url_clicks(shorten_url):
     }
     url = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(shorten_url)
     response = requests.get(url, headers=headers, params=params)
-    if response.ok:
-        data = response.json()
-        total_clicks = data['total_clicks']
-    else:
-        total_clicks = "Incorrect url"
+    if not response.ok:
+        return
+    data = response.json()
+    total_clicks = data['total_clicks']
     return total_clicks
 
 
 def is_bitlink(url):
-    """
-    Ф-ия принимает url
-    Ф-ия возвращает true/false, если url битлинк/не битлинк
-    """
+    """Return True if url is bitlink"""
     headers = {
         'Authorization': 'Bearer {}'.format(token)
     }
     url = 'https://api-ssl.bitly.com/v4/bitlinks/{}'.format(url)
     response = requests.get(url, headers=headers)
-    if response.ok:
-        return True
-    else:
-        return False
+    return response.ok
 
 
 if __name__ == "__main__":
@@ -78,3 +64,11 @@ if __name__ == "__main__":
     else:
         shorten_url = get_shorten_url(token, input_url)
         print(shorten_url)
+
+
+    # def test():
+    #     r = requests.get('https://www.detmir.r')
+    #     return r.ok
+
+    # res = test()
+    # print(res)
